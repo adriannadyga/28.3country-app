f3be996a7fd964f5063eb70bf97c8a85d0fbf55a
Adrianna Dyga <adrianna.dyga@gmail.com>
2019-11-17 08:31:39 +0100

    Add functionality - country list, delete chosen country, show countries of chosen continent

--- src/reducers/countries-reducer.js
+++ src/reducers/countries-reducer.js
@@ -1,21 +1,39 @@
 //(2)stworzenie reducera
 //import potrzebnej akcji
-import { GET_COUNTRIES } from '../actions/countries';
+import { GET_COUNTRIES, GET_COUNTRY, SEARCH_COUNTRIES, DELETE_COUNTRY, SET_CONTINENT } from '../actions/countries';
 //import danych z pliku json
 import countriesData from '../data/countries.json';
 
 //stworzenie inicjalnego stanu aplikacji
 const initialState = {
-    countries: countriesData
+    countries: countriesData,
+    selectedCountry: {},
+    visibleCountries: []
 };
 
 //stworzenie i import reducera
 //state = initialState - zabezpieczenie w razie wartości undefined przypisanie do state initialState
 const countriesReducer = function (state = initialState, action) {
     switch (action.type) {
-        //po wywołaniu metody reducer tworzy kopię obiektu state i do pola countries przypisuje obecną wartość pola
         case GET_COUNTRIES:
-            return Object.assign({}, state, {countries: state.countries})
+            return Object.assign({}, state, {countries: state.countries});
+
+        case GET_COUNTRY:
+            const selectedCountry = state.countries.find(country => country.id == action.id);
+            return Object.assign({}, state, {selectedCountry});
+
+        case SEARCH_COUNTRIES:
+            const foundCountries = state.countries.filter(country => country.name.toLowerCase().includes(action.searchText.toLowerCase()));
+            return Object.assign({}, state, {visibleCountries: foundCountries});
+
+        case DELETE_COUNTRY:
+            const notDeletedCountries = state.countries.filter(country => country.id != action.id);
+            const notDeletedVisibleCountries = state.visibleCountries.filter(country => country.id != action.id);
+            return Object.assign({}, state, {countries: notDeletedCountries, visibleCountries: notDeletedVisibleCountries});
+
+        case SET_CONTINENT:
+            const continentCountries = state.countries.filter(country => country.continent === action.name);
+            return Object.assign({}, state, {visibleCountries: continentCountries});
     }
 
     return state;
